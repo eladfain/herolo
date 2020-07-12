ada78539a4915bc1124223277588673e50c6d99c
nirit fain <eladfain@gmail.com>
2020-07-12 09:50:55 +0300

    update key

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,2 +1,2 @@
-export const WEATHER_KEY="U8wkURDahp3bjXfgr22f2zW0jHyyhfL8";
+export const WEATHER_KEY="rxAVPZ5uMlnqHylT6ZfI0cg7fxPsnnfT";
 export const ICON_URL="https://developer.accuweather.com/sites/default/files/${icon}-s.png";
